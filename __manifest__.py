8cc319b6eb5bcd4d7e13868e2b58b759dc2500d0
gabrieljs26 <gabrieljs26.12@gmail.com>
2025-11-12 15:43:50 +0100

    nuevo

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Gestión/Usuarios',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -22,11 +22,11 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
+    
     'demo': [
         'demo/demo.xml',
     ],
