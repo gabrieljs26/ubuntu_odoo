0c825b7f1f27fe64b1ea75e70f497dc1cd7f5780
gabrieljs26 <gabrieljs26.12@gmail.com>
2025-11-12 17:18:59 +0100

    hola

--- models/modelo_ejemplo.py
+++ models/modelo_ejemplo.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
+
 class EjemploModelo(models.Model):
     _name = 'ejemplo.modelo'
     _description = 'Modelo de Ejemplo'
 
     name = fields.Char(string='Nombre')
-    
