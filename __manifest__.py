bcae12fc21ee8584e965f755b1e98c847aabdc4b
gabrieljs26 <gabrieljs26.12@gmail.com>
2025-11-17 17:43:53 +0100

    hola

--- __manifest__.py
+++ __manifest__.py
@@ -14,8 +14,9 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Human Resources/Employees',
+'category': 'Human Resources/Employees',
     'version': '0.1',
+    'application': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
